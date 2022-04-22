26472356a4f7d8f6f72b2e31fb17d9fa14c40f64
Sid Murthy <murthy.siddhant@googlemail.com>
2022-04-22 18:47:44 +0100

    Reading only the ID and doing so in a loop

--- read.py
+++ read.py
@@ -6,9 +6,10 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        print("Waiting for you to scan an RFID sticker/card")
-        id = reader.read()[0]
-        print("The ID for this card is:", id)
+        while(True):
+                #print("Waiting for you to scan an RFID sticker/card")
+                id = reader.read_id()
+                print("The ID for this card is:", id)
         
 finally:
         GPIO.cleanup()
