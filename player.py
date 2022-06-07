b179fb2538b0a813f3594bec05d2bd45722fcd3d
Sid Murthy <murthy.siddhant@googlemail.com>
2022-06-07 20:53:24 +0100

    Revert "adding logging"
    This reverts commit 4e666e31786fd8d89bfed4bd4d1f17c07203359a.

--- player.py
+++ player.py
@@ -1,25 +1,27 @@
 '''Execute this script to run the player'''
+
+#TODO: Make song not start from the beginning each time it reads a tag
 #TODO: Specify Device ID/think about what happens if no device is active
     #This isnt possible. I will have to make the rpi an active device...
 #TODO: Add logger
 
+from pickle import TRUE
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
-import logging 
 
-#load environment variables
 CLIENT_ID = os.getenv("VINYL_CLIENT_ID")
 CLIENT_SECRET = os.getenv("VINYL_CLIENT_SECRET")
 DEVICE_ID = os.getenv("VINY_DEVICE_ID")
 
-#dictionary to relate an RFID tag to Spotify URI
-id_to_track = {584188076415:'spotify:track:3fWMFwaJil8fD14JCgwbcL' #aoge jab tum
-               } 
+id_to_track = {703790094799:'spotify:track:043dDJ9u0PQ3ooAXMgcwOe',
+               565828783159:'spotify:track:0gplL1WMoJ6iYaPgMCL0gX'} 
 
-#setup spotify auth
+
+
+
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -27,17 +29,10 @@ sp = spotipy.Spotify(
         redirect_uri="http://google.com/",
         scope="user-read-playback-state,user-modify-playback-state"))
 
-#setup the RFID
 reader = SimpleMFRC522()
 
-#setup variables
 prev_id = None
-
-#setup logging
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
-#main loop
-while False:
+while True:
     try:
         print("waiting for rfid")
         id = reader.read_id()
